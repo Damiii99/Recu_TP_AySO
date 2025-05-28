da1c2c9bf89f3f7f65e458e600035507c78e0a58
Damiii99 <damiarojas22@gmail.com>
2025-05-28 07:58:44 -0300

    ADD: Punto D

---
+++ Punto_d.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/Ejercicio_d/{rx,tx}
+for i in {1..20}; do
+mkdir ~/Ejercicio_d/rx/lotes_$i
+mkdir ~/Ejercicio_d/tx/lotes_$i
+done
+for i in {1..100}; do
+mkdir ~/Ejercicio_d/rx/extras_$i
+done
